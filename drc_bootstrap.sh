028e49c13005c0386c3efc309f58fb9b62b5c8c2
hauptmech <hauptmech@gmail.com>
2013-08-22 13:43:41 +0200

    Add missing REPOSITORY variable. Add documentation

--- drc_bootstrap.sh
+++ drc_bootstrap.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# The drc bootstrap script performs the minimal changes necessary for 
+# using the drc binaries repository and doing development for the drc.
+# 
+# The script will do no harm to a new system or and existing system.
+#
+# It 1: Adds the repository server ip to the /etc/hosts file.
+#    2: Adds the repository server name and subfolder to the apt sources.list
+#    3: Runs 'apt-get update' and installs the teh-build-tools script
+
 SERVER_IP=90.147.26.149
 SERVER_USER=drc
 SERVER_NAME=debiangpm
+REPOSITORY=raring_drc
 
 # 1. Set drc server
 sudo sh -c "echo \"$SERVER_IP   $SERVER_NAME\" >> /etc/hosts"
@@ -14,7 +24,7 @@ sudo cp ~/.drc.list /etc/apt/sources.list.d/drc.list
 sudo apt-get update
 
 
-# 3. apt-get dependencies
+# 3. apt-get dependencies, install teh build script.
 
 sudo apt-get install build-essential packaging-dev
 sudo apt-get install teh-build-tools
