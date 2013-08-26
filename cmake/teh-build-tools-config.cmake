1914d64a19c6927d2c2c1f7d447eab17e3fcab47
hauptmech <hauptmech@gmail.com>
2013-08-26 18:21:54 +0200

    Refine functionality of teh-build-tools.cmake

--- cmake/teh-build-tools-config.cmake
+++ cmake/teh-build-tools-config.cmake
@@ -1,58 +1,67 @@
 
-macro (teh_create_cmake_config LIBRARY_NAME INCLUDE_PATH_INDICATOR_FILE)
-# Create our ${LIBRARY_NAME}Config.cmake file
-
-configure_file(@CMAKE_INSTALL_PREFIX@/share/teh-build-tools/cmake/libConfig.cmake.in 
-              "${LIBRARY_NAME}-config.cmake" @ONLY)
-install(FILES "${LIBRARY_NAME}-config.cmake" DESTINATION lib/${LIBRARY_NAME})
+macro (teh_create_cmake_config LIBRARY_NAME INC_PATH_INDICATOR_FILE)
+	# Create our ${LIBRARY_NAME}Config.cmake file
+	set(INCLUDE_PATH_INDICATOR_FILE ${INC_PATH_INDICATOR_FILE})
+	if(NOT LIBRARY_NAME)
+	message(FATAL_ERROR "
+	LIBRARY_NAME must be set
+	")
+	endif()
+	configure_file(@CMAKE_INSTALL_PREFIX@/share/teh-build-tools/cmake/libConfig.cmake.in 
+				  "${LIBRARY_NAME}Config.cmake" @ONLY)
+	install(FILES "${LIBRARY_NAME}Config.cmake" DESTINATION lib/${LIBRARY_NAME})
 endmacro()
 
-macro (teh_add_dependency LIBNAME)
-if (${LIBNAME}_LIBRARIES)
-message("Adding library dependency: ${LIBNAME} ${${LIBNAME}_LIBRARIES}")
-list(APPEND DEPENDENCY_LIBRARIES "${${LIBNAME}_LIBRARIES}")
-endif()
-if (${LIBNAME}_INCLUDE_DIRS)
-message("Adding include files dependency: ${LIBNAME} ${${LIBNAME}_LIBRARIES}")
-list(APPEND DEPENDENCY_INCLUDE_DIRS "${${LIBNAME}_INCLUDE_DIRS}")
-endif()
+macro (teh_add_to_config LIBNAME)
+	if (TARGET ${LIBNAME})
+		message("Adding library: ${LIBNAME}")
+		LIST(APPEND TEH_MY_LIBRARIES ${LIBNAME})
+	endif()
+	if (${LIBNAME}_LIBRARIES)
+	message("Adding library dependency: ${LIBNAME} ${${LIBNAME}_LIBRARIES}")
+	list(APPEND DEPENDENCY_LIBRARIES "${${LIBNAME}_LIBRARIES}")
+	endif()
+	if (${LIBNAME}_INCLUDE_DIRS)
+	message("Adding include files dependency: ${LIBNAME} ${${LIBNAME}_LIBRARIES}")
+	list(APPEND DEPENDENCY_INCLUDE_DIRS "${${LIBNAME}_INCLUDE_DIRS}")
+	endif()
 endmacro()
 
 
 macro (teh_find_libraries_in_development)
-# Specify where to search for local *Config.cmake and FindXXX.cmake files.
-find_file(DEVROOT .development_root PATHS ${CMAKE_SOURCE_DIR}/.. PATH_SUFFIXES .. ../.. NO_DEFAULT_PATH)
-if(${DEVROOT} MATCHES "DEVROOT-NOTFOUND")
-    message("[WARNING] .development_root marker file not found.")
-    set(DEVROOT ${CMAKE_SOURCE_DIR}/..)
-else()
-    get_filename_component(DEVROOT ${DEVROOT} PATH)
-endif()
+	# Specify where to search for local *Config.cmake and FindXXX.cmake files.
+	find_file(DEVROOT .development_root PATHS ${CMAKE_SOURCE_DIR}/.. PATH_SUFFIXES .. ../.. NO_DEFAULT_PATH)
+	if(${DEVROOT} MATCHES "DEVROOT-NOTFOUND")
+		message("[WARNING] .development_root marker file not found.")
+		set(DEVROOT ${CMAKE_SOURCE_DIR}/..)
+	else()
+		get_filename_component(DEVROOT ${DEVROOT} PATH)
+	endif()
 
-#find active projects
-file(GLOB ACTIVE_LIBRARY_PATHS "${DEVROOT}/*/build" "${DEVROOT}/*/*/build" "${DEVROOT}/*/*/*/build")
+	#find active projects
+	file(GLOB ACTIVE_LIBRARY_PATHS "${DEVROOT}/*/build" "${DEVROOT}/*/*/build" "${DEVROOT}/*/*/*/build")
 
-# Search for config files buried in active projects
-unset(REALLY_LOCAL_FIND_LIBRARY_PATHS)
-foreach(SRCH IN LISTS ACTIVE_LIBRARY_PATHS)
-    file(GLOB_RECURSE CONFFILES ${SRCH}/*Config.cmake)
-    foreach(conffile IN LISTS CONFFILES)
-        get_filename_component(res ${conffile} PATH)
-        list(APPEND REALLY_LOCAL_FIND_LIBRARY_PATHS ${res})
-    endforeach()
-endforeach()
+	# Search for config files buried in active projects
+	unset(REALLY_LOCAL_FIND_LIBRARY_PATHS)
+	foreach(SRCH IN LISTS ACTIVE_LIBRARY_PATHS)
+		file(GLOB_RECURSE CONFFILES ${SRCH}/*Config.cmake)
+		foreach(conffile IN LISTS CONFFILES)
+			get_filename_component(res ${conffile} PATH)
+			list(APPEND REALLY_LOCAL_FIND_LIBRARY_PATHS ${res})
+		endforeach()
+	endforeach()
 
-list(REMOVE_DUPLICATES REALLY_LOCAL_FIND_LIBRARY_PATHS) 
+	list(REMOVE_DUPLICATES REALLY_LOCAL_FIND_LIBRARY_PATHS) 
 
-message("== Search paths for: ${CMAKE_CURRENT_SOURCE_DIR} ==")
-foreach(PTH IN LISTS REALLY_LOCAL_FIND_LIBRARY_PATHS)
-message("[] ${PTH}")
-endforeach()
+	message("== Search paths for: ${CMAKE_CURRENT_SOURCE_DIR} ==")
+	foreach(PTH IN LISTS REALLY_LOCAL_FIND_LIBRARY_PATHS)
+	message("[] ${PTH}")
+	endforeach()
 
-list(APPEND LOCAL_FIND_LIBRARY_PATHS  ${REALLY_LOCAL_FIND_LIBRARY_PATHS})
+	list(APPEND LOCAL_FIND_LIBRARY_PATHS  ${REALLY_LOCAL_FIND_LIBRARY_PATHS})
 
-set(CMAKE_PREFIX_PATH ${LOCAL_FIND_LIBRARY_PATHS})
-set(CMAKE_MODULE_PATH "${DEVROOT}/CMakeModules")
+	set(CMAKE_PREFIX_PATH ${LOCAL_FIND_LIBRARY_PATHS})
+	set(CMAKE_MODULE_PATH "${DEVROOT}/CMakeModules")
 endmacro()
 
 
