ec6ddaed6e2322349260fe22d80967ba82eda0c7
hauptmech <hauptmech@gmail.com>
2013-08-26 21:50:36 +0200

    Sync libConfig.cmake with explicit approach

--- cmake/teh-build-tools-config.cmake
+++ cmake/teh-build-tools-config.cmake
@@ -1,34 +1,50 @@
-
-macro (teh_create_cmake_config LIBRARY_NAME INC_PATH_INDICATOR_FILE)
+#macro (
+macro (teh_create_config_file MY_LIBRARY_NAME INC_PATH_INDICATOR_FILE INC_PATH_SUFFIX)
 	# Create our ${LIBRARY_NAME}Config.cmake file
 	set(INCLUDE_PATH_INDICATOR_FILE ${INC_PATH_INDICATOR_FILE})
+	set(TEH_LIBRARY_NAME ${MY_LIBRARY_NAME})
+	set(TEH_INC_SUFFIX ${INC_PATH_SUFFIX})
 	if(NOT LIBRARY_NAME)
-	message(FATAL_ERROR "
-	LIBRARY_NAME must be set
-	")
+		message(FATAL_ERROR " the variable $LIBRARY_NAME must be set to the name of this library! ")
+	endif()
+	if(NOT INCLUDE_PATH_INDICATOR_FILE)
+		message(FATAL_ERROR " the variable $INCLUDE_PATH_INDICATOR_FILE must be set to an include file that can be used in searches to identify the location of this package when installed! ")
 	endif()
 	configure_file(@CMAKE_INSTALL_PREFIX@/share/teh-build-tools/cmake/libConfig.cmake.in 
 				  "${LIBRARY_NAME}Config.cmake" @ONLY)
 	install(FILES "${LIBRARY_NAME}Config.cmake" DESTINATION lib/${LIBRARY_NAME})
 endmacro()
 
-macro (teh_add_to_config LIBNAME)
+macro (teh_target LIBNAME)
 	if (TARGET ${LIBNAME})
 		message("Adding library: ${LIBNAME}")
-		LIST(APPEND TEH_MY_LIBRARIES ${LIBNAME})
-	endif()
-	if (${LIBNAME}_LIBRARIES)
-	message("Adding library dependency: ${LIBNAME} ${${LIBNAME}_LIBRARIES}")
-	list(APPEND DEPENDENCY_LIBRARIES "${${LIBNAME}_LIBRARIES}")
-	endif()
-	if (${LIBNAME}_INCLUDE_DIRS)
-	message("Adding include files dependency: ${LIBNAME} ${${LIBNAME}_LIBRARIES}")
-	list(APPEND DEPENDENCY_INCLUDE_DIRS "${${LIBNAME}_INCLUDE_DIRS}")
+		LIST(APPEND TEH_EXPORTED_LIBRARIES ${LIBNAME})
+	else()
+		message(FATAL_ERROR "${LIBNAME} is not an active target. Perhaps you need to create it with 'add_libary' or 'add_executable'?"
 	endif()
 endmacro()
 
+macro (teh_package LIBNAME)
+	if (${LIBNAME}_LIBRARIES)
+	message("Adding library dependency: ${LIBNAME} ${${LIBNAME}_LIBRARIES}")
+	list(APPEND TEH_DEPENDENCY_LIBRARIES "${${LIBNAME}_LIBRARIES}")
+	endif()
+	if (${LIBNAME}_INCLUDE_DIRS)
+	message("Adding include files dependency: ${LIBNAME} ${${LIBNAME}_INCLUDE_DIRS}")
+	list(APPEND TEH_DEPENDENCY_INCLUDE_DIRS "${${LIBNAME}_INCLUDE_DIRS}")
+	endif()
+endmacro()
 
-macro (teh_find_libraries_in_development)
+macro (teh_print_search)
+	message("=== Search paths for: ${CMAKE_CURRENT_SOURCE_DIR} ")
+	foreach(PTH IN LISTS REALLY_LOCAL_FIND_LIBRARY_PATHS)
+		message("[] ${PTH}")
+	endforeach()
+	message("===================================================")
+endmacro()
+
+#macro (teh_search_local
+macro (teh_search)
 	# Specify where to search for local *Config.cmake and FindXXX.cmake files.
 	find_file(DEVROOT .development_root PATHS ${CMAKE_SOURCE_DIR}/.. PATH_SUFFIXES .. ../.. NO_DEFAULT_PATH)
 	if(${DEVROOT} MATCHES "DEVROOT-NOTFOUND")
